dab1c110c23bd79e92958312fddf72f29e340700
Ronal James <92068615+Jamesllm@users.noreply.github.com>
2023-06-16 17:06:34 -0500

    1.1

--- tvgo.py
+++ tvgo.py
@@ -2,36 +2,43 @@ import pyautogui
 import time
 
 #size
-print(pyautogui.size())
-sizeX = 1280
-sizeY = 720
+screen_width, screen_height = pyautogui.size()
 
 def halfScreen(): 
-    percentX = sizeX-(sizeX*0.5)
-    percentY = sizeY-(sizeY*0.5)
-    return percentX, percentY
-
-def calculateUbicationButtonFullSize():
-    percentX = sizeX-(sizeX*0.093)
-    percentY = sizeY-(sizeY*0.082)
+    percentX = screen_width-(screen_width*0.5)
+    percentY = screen_height-(screen_height*0.5)
     return percentX, percentY
 
 pyautogui.hotkey('win', 'r')
 time.sleep(1.5)
 pyautogui.write('chrome.exe "https://tvgo.americatv.com.pe/"', interval=0.01)
-time.sleep(1.5)
+time.sleep(0.5)
 pyautogui.press('enter')
 time.sleep(3)
 
 #click - X and Y
 pyautogui.click(halfScreen())
 time.sleep(2)
-#scroll down sizeY-(sizeY*0.5)-20
-pyautogui.scroll(-340)
-time.sleep(2)
 
-#0.093  0.95% -- x
-#0.082  0.85% -- y
-pyautogui.click(calculateUbicationButtonFullSize())
+if (screen_width == 1280) and (screen_height == 720):
+    #scroll down sizeY-(sizeY*0.5)-20
+    pyautogui.scroll(-340)
+    time.sleep(2)
+    #0.093  0.95% -- x
+    #0.082  0.85% -- y
+    pyautogui.click(1160, 660)
+
+elif (screen_width == 1600) and (screen_height == 900):
+    pyautogui.scroll(-348)
+    time.sleep(2)
+    pyautogui.click(1500, 820)
+
+elif (screen_width == 1920) and (screen_height == 1080):
+    pyautogui.scroll(-355)
+    time.sleep(2)
+    pyautogui.click(1800, 1000)
+
+time.sleep(1.5)
+pyautogui.moveTo(halfScreen())
 time.sleep(0.5)
 pyautogui.moveTo(halfScreen())
